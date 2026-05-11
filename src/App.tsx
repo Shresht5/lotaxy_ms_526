401f4cd90d733db7758578fb16872ba1c7b0dc92
Shreshta <117246220+Shresht5@users.noreply.github.com>
2026-05-11 08:09:14 +0530

    react add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './page/Home';
+import { About } from './page/About';
+import './index.css'
 
 export default () => {
     return (
-        <div>App</div>
+        <MemoryRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </MemoryRouter>
     )
 }
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ if (started) {
 }
 
 const createWindow = () => {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -23,6 +22,7 @@ const createWindow = () => {
   } else {
     mainWindow.loadFile(
       path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`),
+
     );
   }
 
